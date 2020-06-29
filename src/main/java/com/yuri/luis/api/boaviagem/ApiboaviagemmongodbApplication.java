8290d7765c11480a285d94a0b7b3f8525b6c1aa3
YuriLuis <yuri.luizg@hotmail.com>
2020-06-29 16:28:47 -0300

    Projeto criado

---
+++ src/main/java/com/yuri/luis/api/boaviagem/ApiboaviagemmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.boaviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiboaviagemmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiboaviagemmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/luis/api/boaviagem/ApiboaviagemmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.api.boaviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiboaviagemmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
